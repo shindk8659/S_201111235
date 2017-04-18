29113d843a2bbe6846eb8f583abbec4167d3f90f
shindk8659 <shindk8659@naver.com>
2017-04-18 16:15:58 +0900

    delete key

--- 201111235/src/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-import mylib
-
-keyPath=os.path.join(os.getcwd(), 'key.properties')
-key=mylib.getKey(keyPath)
--- 201111235/src/mylib.py
+++
@@ -1,9 +0,0 @@
-
-def getKey(keyPath):
-    d=dict()
-    f=open(keyPath,'r')
-    for line in f.readlines():
-        row=line.split('=')
-        row0=row[0]
-        d[row0]=row[1].strip()
-    return d
